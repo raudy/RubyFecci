be4293d31ea296d10d09cc7a8552116ae6a64bf4
Katherine Saez <kangel_ksb@hotmail.com>
2015-11-05 15:23:26 -0300

    Eliminando Basura

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Empleado.create(user: 'fernando', pass: 'fernando')
+Empleado.create(user: 'fernando', pass: 'fernando' )
